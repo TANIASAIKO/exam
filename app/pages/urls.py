3a48ae479c0e24fe4d5998584b0e31b46fc729b1
TANIASAIKO <tatana155555@gmail.com>
2023-02-23 02:57:10 +0300

    add part of app

--- app/pages/urls.py
+++ app/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tags/<slug:tag_name>', views.index, name='tag'),
+    path('description', views.description, name='description'),
 ]
--- app/pages/views.py
+++ app/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 TEST_TAG_ITEMS =  [
-    { 'tag': 'warning', 'parent_tag': None, 'title': 'title', 'description': 'some description', 'menu_type': 'common' },
+    { 'tag': 'warning', 'parent_tag': None, 'title': 'title', 'description': 'some description' },
     { 'tag': 'warning2', 'parent_tag': 'warning', 'title': 'title', 'description': 'some description' },
     { 'tag': 'warning3', 'parent_tag': 'warning', 'title': 'title', 'description': 'some description' },
     { 'tag': 'warning1', 'parent_tag': 'warning', 'title': 'title', 'description': 'some description' },
@@ -12,45 +12,40 @@ TEST_TAG_ITEMS =  [
     { 'tag': 'warning8', 'parent_tag': 'warning', 'title': 'title', 'description': 'some description' },
     { 'tag': 'warning9', 'parent_tag': 'warning', 'title': 'title', 'description': 'some description' },
     
-    { 'tag': 'shopping_cart', 'parent_tag': None, 'title': 'title', 'description': 'some description', 'menu_type': 'common' },
-    { 'tag': 'shopping_cart1', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart2', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart3', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart4', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart5', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart6', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart7', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart8', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'shopping_cart9', 'parent_tag': 'shopping_cart', 'title': 'title', 'description': 'some description' },
-    
-    { 'tag': 'sell', 'parent_tag': None, 'title': 'title', 'description': 'some description', 'menu_type': 'common' },
-    { 'tag': 'sell1', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell2', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell3', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell4', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell5', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell6', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell7', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell8', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'sell9', 'parent_tag': 'sell', 'title': 'title', 'description': 'some description' },
-    
-    { 'tag': 'emergency', 'parent_tag': None, 'title': 'title', 'description': 'some description', 'menu_type': 'extra' },
-    { 'tag': 'emergency1', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency2', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency3', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency4', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency5', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency6', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency7', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency8', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
-    { 'tag': 'emergency9', 'parent_tag': 'emergency', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous', 'parent_tag': None, 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous1', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous2', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous3', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous4', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous5', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous6', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous7', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous8', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+    { 'tag': 'dangerous9', 'parent_tag': 'dangerous', 'title': 'title', 'description': 'some description' },
+]
+
+EXTRA_MENU_ITEMS = [
+    { 'icon_name': 'description', 'url_name': 'description' },
+    # { 'icon_name': 'logout', 'url_name': 'logout' },
 ]
 
 def index(request, tag_name = None):
     ctx = {
-        'menu_items': [*filter(lambda i: i.get('menu_type') is not None, TEST_TAG_ITEMS)],
+        'menu_items': [*filter(lambda i: i.get('parent_tag') is None, TEST_TAG_ITEMS)],
+        'extra_menu_items': EXTRA_MENU_ITEMS,
         'board_items': [*filter(lambda i: tag_name is None or i.get('parent_tag', None) == tag_name, TEST_TAG_ITEMS)],
-        'current': next(item for item in TEST_TAG_ITEMS if item.get('tag', None) == tag_name)
+        'current': next(item for item in TEST_TAG_ITEMS if item.get('tag', None) == tag_name) if tag_name else None
     }
     
     return render(request, 'pages/index.html', ctx)
+
+def description(request):
+    ctx = {
+        'menu_items': [*filter(lambda i: i.get('parent_tag') is None, TEST_TAG_ITEMS)],
+        'extra_menu_items': EXTRA_MENU_ITEMS,
+    }
+    
+    return render(request, 'pages/description.html', ctx)
+
+# def logout(request):
+#     return render(request, 'pages/index.html')
