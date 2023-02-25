0a455c63726a67ce9004b6f8bfa712b4d423a3ef
TANIASAIKO <tatana155555@gmail.com>
2023-02-25 12:32:56 +0300

    add django app

--- app/pages/views.py
+++ app/pages/views.py
@@ -6,6 +6,36 @@ EXTRA_MENU_ITEMS = [
     { 'icon_name': 'logout', 'url_name': 'logout' },
 ]
 
+USER_INFO = [
+    { 'icon': 'fa-user', 'label': 'Full Name', 'value': 'Tatiana Saiko' },
+    { 'icon': 'fa-envelope', 'label': 'Email', 'value': 'tatana155555@gmail.com' },
+    { 'icon': 'fa-mobile', 'label': 'Mobile', 'value': '+375 (29) 279-18-29' },
+    { 'icon': 'fa-phone', 'label': 'Phone', 'value': '+375 (17) 395-17-42' },
+    { 'icon': 'fa-map-marker', 'label': 'Address', 'value': 'Belarus, Minsk' },
+]
+
+TECHNOLOGIES = [
+    { 'label': 'Python / Django', 'progress_start': 0, 'duration': 50 },
+    { 'label': 'Bootstrap', 'progress_start': 50, 'duration': 35 },
+    { 'label': 'Html', 'progress_start': 85, 'duration': 10 },
+    { 'label': 'Google fonts / icons', 'progress_start': 95, 'duration': 5 },
+]
+
+TOOLS = [
+    { 'label': 'PyCharm', 'progress_start': 0, 'duration': 50 },
+    { 'label': 'Google Chrome', 'progress_start': 50, 'duration': 35 },
+    { 'label': 'Git', 'progress_start': 85, 'duration': 10 },
+    { 'label': 'Virtualenv', 'progress_start': 95, 'duration': 5 },
+]
+
+CONTACTS = [
+    { 'icon': 'fa-github', 'color': '#333333', 'link': 'https://github.com/TANIASAIKO', 'label': 'TANIASAIKO' },
+    { 'icon': 'fa-telegram', 'color': '#55acee', 'link': 'https://t.me/TatsianaSaiko', 'label': '@TatsianaSaiko' },
+    { 'icon': 'fa-instagram', 'color': '#55acee', 'link': 'https://instagram.com/tatana155555?igshid=ZDdkNTZiNTM=', 'label': 'tatana155555' },
+    { 'icon': 'fa-vk', 'color': '#55acee', 'link': 'https://vk.com/id50512221', 'label': 'id50512221' },
+    { 'icon': 'fa-envelope', 'color': '#ffc107', 'link': 'tatana155555@gmail.com', 'label': 'tatana155555@gmail.com' },
+]
+
 def index(request):
     if not request.user.is_authenticated:
         return redirect('/accounts/login/')
@@ -13,6 +43,10 @@ def index(request):
     ctx = {
         'menu_items': Card.objects.filter(rank='2'),
         'extra_menu_items': EXTRA_MENU_ITEMS,
+        'contacts': CONTACTS,
+        'tools': TOOLS,
+        'technologies': TECHNOLOGIES,
+        'user_info': USER_INFO,
     }
     
     return render(request, 'pages/profile.html', ctx)
